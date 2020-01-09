02260ba59d1f34351b8dea905a712d72a7bef357
MMMaellon <mmmaellon@gmail.com>
2020-01-09 02:13:59 -0800

    more boilerplate

--- renik.cpp
+++ renik.cpp
@@ -2,35 +2,43 @@
 
 RenIK::RenIK(){};
 
+void RenIK::_init(){
+
+}
+
+void RenIK::_ready(){
+
+}
+
 void RenIK::_bind_methods() {
 	ADD_GROUP("Armature", "armature_");
-	ADD_PROPERTY(PropertyInfo(Variant::NODE_PATH, "Skeleton"), "set_skeleton_path", "get_skeleton_path");
+	ADD_PROPERTY(PropertyInfo(Variant::NODE_PATH, "Skeleton"), "set_skeleton_path", "get_skeleton_path_name");
 
-	ADD_PROPERTY(PropertyInfo(Variant::STRING, "Head Bone"), "set_head_bone", "get_head_bone");
-	ADD_PROPERTY(PropertyInfo(Variant::POOL_STRING_ARRAY, "Neck Bones"), "set_neck_bones", "get_neck_bones");
-	ADD_PROPERTY(PropertyInfo(Variant::STRING, "Shoulder Left Bone"), "set_shoulder_left_bone", "get_shoulder_left_bone");
-	ADD_PROPERTY(PropertyInfo(Variant::STRING, "Upper Arm Left Bone"), "set_upper_arm_left_bone", "get_upper_arm_left_bone");
-	ADD_PROPERTY(PropertyInfo(Variant::STRING, "Forearm Left Bone"), "set_forearm_left_bone", "get_forearm_left_bone");
-	ADD_PROPERTY(PropertyInfo(Variant::STRING, "Hand Left Bone"), "set_hand_left_bone", "get_hand_left_bone");
-	ADD_PROPERTY(PropertyInfo(Variant::STRING, "Shoulder Right Bone"), "set_shoulder_right_bone", "get_shoulder_right_bone");
-	ADD_PROPERTY(PropertyInfo(Variant::STRING, "Upper Arm Right Bone"), "set_upper_arm_right_bone", "get_upper_arm_right_bone");
-	ADD_PROPERTY(PropertyInfo(Variant::STRING, "Forearm Right Bone"), "set_forearm_right_bone", "get_forearm_right_bone");
-	ADD_PROPERTY(PropertyInfo(Variant::STRING, "Hand Right Bone"), "set_hand_right_bone", "get_hand_right_bone");
-	ADD_PROPERTY(PropertyInfo(Variant::POOL_STRING_ARRAY, "Spine Bones"), "set_spine_bones", "get_spine_bones");
-	ADD_PROPERTY(PropertyInfo(Variant::STRING, "Hip Bone"), "set_hip_bone", "get_hip_bone");
-	ADD_PROPERTY(PropertyInfo(Variant::STRING, "Thigh Left Bone"), "set_thigh_left_bone", "get_thigh_left_bone");
-	ADD_PROPERTY(PropertyInfo(Variant::STRING, "Shin Left Bone"), "set_shin_left_bone", "get_shin_left_bone");
-	ADD_PROPERTY(PropertyInfo(Variant::STRING, "Foot Left Bone"), "set_foot_left_bone", "get_foot_left_bone");
-	ADD_PROPERTY(PropertyInfo(Variant::STRING, "Thigh Right Bone"), "set_thigh_right_bone", "get_thigh_right_bone");
-	ADD_PROPERTY(PropertyInfo(Variant::STRING, "Shin Right Bone"), "set_shin_right_bone", "get_shin_right_bone");
-	ADD_PROPERTY(PropertyInfo(Variant::STRING, "Foot Right Bone"), "set_foot_right_bone", "get_foot_right_bone");
+	ADD_PROPERTY(PropertyInfo(Variant::STRING, "Head Bone"), "set_head_bone", "get_head_bone_name");
+	ADD_PROPERTY(PropertyInfo(Variant::POOL_STRING_ARRAY, "Neck Bones"), "set_neck_bones", "get_neck_bones_names");
+	ADD_PROPERTY(PropertyInfo(Variant::STRING, "Shoulder Left Bone"), "set_shoulder_left_bone", "get_shoulder_left_bone_name");
+	ADD_PROPERTY(PropertyInfo(Variant::STRING, "Upper Arm Left Bone"), "set_upper_arm_left_bone", "get_upper_arm_left_bone_name");
+	ADD_PROPERTY(PropertyInfo(Variant::STRING, "Forearm Left Bone"), "set_forearm_left_bone", "get_forearm_left_bone_name");
+	ADD_PROPERTY(PropertyInfo(Variant::STRING, "Hand Left Bone"), "set_hand_left_bone", "get_hand_left_bone_name");
+	ADD_PROPERTY(PropertyInfo(Variant::STRING, "Shoulder Right Bone"), "set_shoulder_right_bone", "get_shoulder_right_bone_name");
+	ADD_PROPERTY(PropertyInfo(Variant::STRING, "Upper Arm Right Bone"), "set_upper_arm_right_bone", "get_upper_arm_right_bone_name");
+	ADD_PROPERTY(PropertyInfo(Variant::STRING, "Forearm Right Bone"), "set_forearm_right_bone", "get_forearm_right_bone_name");
+	ADD_PROPERTY(PropertyInfo(Variant::STRING, "Hand Right Bone"), "set_hand_right_bone", "get_hand_right_bone_name");
+	ADD_PROPERTY(PropertyInfo(Variant::POOL_STRING_ARRAY, "Spine Bones"), "set_spine_bones", "get_spine_bones_names");
+	ADD_PROPERTY(PropertyInfo(Variant::STRING, "Hip Bone"), "set_hip_bone", "get_hip_bone_name");
+	ADD_PROPERTY(PropertyInfo(Variant::STRING, "Thigh Left Bone"), "set_thigh_left_bone", "get_thigh_left_bone_name");
+	ADD_PROPERTY(PropertyInfo(Variant::STRING, "Shin Left Bone"), "set_shin_left_bone", "get_shin_left_bone_name");
+	ADD_PROPERTY(PropertyInfo(Variant::STRING, "Foot Left Bone"), "set_foot_left_bone", "get_foot_left_bone_name");
+	ADD_PROPERTY(PropertyInfo(Variant::STRING, "Thigh Right Bone"), "set_thigh_right_bone", "get_thigh_right_bone_name");
+	ADD_PROPERTY(PropertyInfo(Variant::STRING, "Shin Right Bone"), "set_shin_right_bone", "get_shin_right_bone_name");
+	ADD_PROPERTY(PropertyInfo(Variant::STRING, "Foot Right Bone"), "set_foot_right_bone", "get_foot_right_bone_name");
 
-	ADD_PROPERTY(PropertyInfo(Variant::NODE_PATH, "Head Target"), "set_head_target_path", "get_head_target_path");
-	ADD_PROPERTY(PropertyInfo(Variant::NODE_PATH, "Left Hand Target"), "set_hand_left_target_path", "get_hand_left_target_path");
-	ADD_PROPERTY(PropertyInfo(Variant::NODE_PATH, "Right Hand Target"), "set_hand_right_target_path", "get_hand_right_target_path");
-	ADD_PROPERTY(PropertyInfo(Variant::NODE_PATH, "Hip Target"), "set_hip_target_path", "get_hip_target_path");
-	ADD_PROPERTY(PropertyInfo(Variant::NODE_PATH, "Left Foot Target"), "set_foot_left_target_path", "get_foot_left_target_path");
-	ADD_PROPERTY(PropertyInfo(Variant::NODE_PATH, "Right Foot Target"), "set_foot_right_target_path", "get_foot_right_target_path");
+	ADD_PROPERTY(PropertyInfo(Variant::NODE_PATH, "Head Target"), "set_head_target_path", "get_head_target_path_name");
+	ADD_PROPERTY(PropertyInfo(Variant::NODE_PATH, "Left Hand Target"), "set_hand_left_target_path", "get_hand_left_target_path_name");
+	ADD_PROPERTY(PropertyInfo(Variant::NODE_PATH, "Right Hand Target"), "set_hand_right_target_path", "get_hand_right_target_path_name");
+	ADD_PROPERTY(PropertyInfo(Variant::NODE_PATH, "Hip Target"), "set_hip_target_path", "get_hip_target_path_name");
+	ADD_PROPERTY(PropertyInfo(Variant::NODE_PATH, "Left Foot Target"), "set_foot_left_target_path", "get_foot_left_target_path_name");
+	ADD_PROPERTY(PropertyInfo(Variant::NODE_PATH, "Right Foot Target"), "set_foot_right_target_path", "get_foot_right_target_path_name");
 
 	ADD_GROUP("Body IK", "body_ik_");
 	ADD_GROUP("Arm IK", "arm_ik_");
@@ -76,43 +84,17 @@ void RenIK::_bind_methods() {
 
 	// register_property<RenIK, bool>("Manual Update", &RenIK::manualUpdate, false);
 
-	// ClassDB::bind_method(D_METHOD("_physics_process"), &RenIK::_physics_process);
-	// ClassDB::bind_method(D_METHOD("_process"), &RenIK::_process);
-	// ClassDB::bind_method(D_METHOD("_ready"), &RenIK::_ready);
-
-	// ClassDB::bind_method(D_METHOD("performBodyIK"), &RenIK::performBodyIK);
-	// ClassDB::bind_method(D_METHOD("performLeftHandIK"), &RenIK::performLeftHandIK);
-	// ClassDB::bind_method(D_METHOD("performRightHandIK"), &RenIK::performRightHandIK);
-	// ClassDB::bind_method(D_METHOD("performLeftFootIK"), &RenIK::performLeftFootIK);
-	// ClassDB::bind_method(D_METHOD("performRightFootIK"), &RenIK::performRightFootIK);
-
-	// ClassDB::bind_method(D_METHOD("setHeadTargetNode"), &RenIK::setHeadTargetNode);
-	// ClassDB::bind_method(D_METHOD("setHandLeftTargetNode"), &RenIK::setHandLeftTargetNode);
-	// ClassDB::bind_method(D_METHOD("setHandRightTargetNode"), &RenIK::setHandRightTargetNode);
-	// ClassDB::bind_method(D_METHOD("setHipTargetNode"), &RenIK::setHipTargetNode);
-	// ClassDB::bind_method(D_METHOD("setFootLeftTargetNode"), &RenIK::setFootLeftTargetNode);
-	// ClassDB::bind_method(D_METHOD("setFootRightTargetNode"), &RenIK::setFootRightTargetNode);
-
-	// ClassDB::bind_method(D_METHOD("setFalling"), &RenIK::setFalling);
-	// ClassDB::bind_method(D_METHOD("setManualUpdate"), &RenIK::setManualUpdate);
-	// ClassDB::bind_method(D_METHOD("update"), &RenIK::update);
+	ClassDB::bind_method(D_METHOD("_physics_process"), &RenIK::_physics_process);
+	ClassDB::bind_method(D_METHOD("_process"), &RenIK::_process);
+	ClassDB::bind_method(D_METHOD("_ready"), &RenIK::_ready);
 }
 
 void RenIK::_validate_property(PropertyInfo &property) const {
 }
 void RenIK::_notification(int p_what) {
 
-	switch (p_what) {
-
-		case NOTIFICATION_ENTER_TREE: {
-
-			_enter_tree();
-		} break;
-		case NOTIFICATION_EXIT_TREE: {
-
-			_exit_tree();
-		} break;
-	}
+	// switch (p_what) {
+	// }
 }
 
 void RenIK::_enter_tree() {
